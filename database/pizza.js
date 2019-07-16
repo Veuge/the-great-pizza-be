c1078056f80ec6575e674b872f88cfc42dca8c1a
Veronica Clavijo <veronica.clavijo@coxautoinc.com>
2019-07-16 19:51:58 -0400

    Added basic sql queries for PizzaService

--- database/pizza.js
+++ database/pizza.js
@@ -1,15 +1,64 @@
 import conn from "./connection";
+import { NotFoundError } from "./errors";
 
 class PizzaService {
   getAll() {
     return new Promise((resolve, reject) => {
-      conn.query("select * from pizza", (err, result) => {
+      conn.query('select * from pizza', (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result)
+        }
+      })
+    });
+  }
+  getById(id) {
+    return new Promise((resolve, reject) => {
+      conn.query('select * from pizza where id = ?', id, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          if (result.length === 0) {
+            reject(new NotFoundError());
+          } else {
+            resolve(result[0]);
+          }
+        }
+      });
+    });
+  }
+  create({name, price}) {
+    return new Promise((resolve, reject) => {
+      conn.query('insert into pizza(name, price) values(?, ?)', [name, price] , (err, result) => {
         if (err) {
           reject(err);
         } else {
           resolve(result);
         }
-      });
+      })
+    });
+  }
+  update(id, {name, price}) {
+    return new Promise((resolve, reject) => {
+      conn.query('update pizza set name=?, price=? where id = ?', [name, price, id], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      })
+    });
+  }
+  delete(id) {
+    return new Promise((resolve, reject) => {
+      conn.query('delete from pizza where id = ?', [id], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      })
     });
   }
 }
