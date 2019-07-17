5e19b8ee332db541a3e3bee608353dadeb036790
Veronica Clavijo <veuge.clavijo@gmail.com>
2019-07-17 14:26:34 -0400

    Add missing methods to ingredient endpoint

--- api/ingredient.js
+++ api/ingredient.js
@@ -11,4 +11,35 @@ router.get('/', asyncMiddleware(async (req, res) => {
   return res.status(200).send(data);
 }));
 
+router.get('/:id', asyncMiddleware(async (req, res, next) => {
+  const ingredientId = req.params.id;
+  const ingredient = await ingredientService.getById(ingredientId);
+  return res.status(200).send(ingredient);
+}));
+
+router.post('/', asyncMiddleware(async (req, res) => {
+  const data = {
+    name: req.body.name
+  };
+  const result = await ingredientService.create(data);
+  return res.status(200).send(result);
+}));
+
+router.delete('/:id', asyncMiddleware(async (req, res) => {
+  const ingredientId = req.params.id;
+  const ingredient = await ingredientService.getById(ingredientId);
+  const result = await ingredientService.delete(ingredient.id);
+  return res.status(200).send(result);
+}));
+
+router.put('/:id', asyncMiddleware(async (req, res) => {
+  const ingredientId = req.params.id;
+  const data = {
+    name: req.body.name
+  };
+  const ingredient = await ingredientService.getById(ingredientId);
+  const result = await ingredientService.update(ingredient.id, data);
+  return res.status(200).send(result);
+}));
+
 export default router;
--- database/ingredient.js
+++ database/ingredient.js
@@ -13,6 +13,56 @@ class IngredientService {
       })
     });
   }
+  getById(id) {
+    return new Promise((resolve, reject) => {
+      conn.query('select * from ingredient where id = ?', id, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          if (result.length === 0) {
+            reject(new NotFoundError());
+          } else {
+            resolve(result[0]);
+          }
+        }
+      });
+    });
+  }
+  create({name}) {
+    return new Promise((resolve, reject) => {
+      conn.query('insert into ingredient(name) values(?)', [name] , async (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      })
+    });
+  }
+  delete(id) {
+    return new Promise((resolve, reject) => {
+      conn.query('delete from ingredient where id = ?', [id], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      })
+    });
+  }
+  update(id, {name}) {
+    return new Promise((resolve, reject) => {
+      conn.query('update ingredient set name=? where id = ?', [name, id], 
+        async (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        }
+      )
+    });
+  }
 }
 
 export default IngredientService;
